adc83fe2cca0bfafe775422fcb0342802e21e3b9
Pamela Ianovalli <pamela.ianovalli@ifood.com.br>
2020-09-06 23:14:42 -0300

    Compose Detail View

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,21 +10,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-                .foregroundColor(.black)
-                .lineLimit(nil)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
-            }
-        }
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
             
-        .padding()
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack(alignment: .top) {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+                
+            .padding()
+            
+            Spacer()
+        }
     }
 }
 
